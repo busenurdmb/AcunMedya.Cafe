edf5aba14b797551f609fc11fbab0888aace0720
busenurdmb <bus.dmb@gmail.com>
2025-04-09 22:28:29 +0300

    adım 5

--- AcunMedya.Cafe/Controllers/ProductController.cs
+++ AcunMedya.Cafe/Controllers/ProductController.cs
@@ -69,5 +69,59 @@ namespace AcunMedya.Cafe.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            List<SelectListItem> values = (from x in _context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }
+                                     ).ToList();
+            ViewBag.v = values;
+
+            var value = _context.Products.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product model)
+        {
+            if (model.ImageFile != null)
+            {
+                //uygulamanın çalıştığı dizini al
+                var currenDirectory = Directory.GetCurrentDirectory();
+
+                //uygulamanın uzantısını al (jpg,png)
+                var extension = Path.GetExtension(model.ImageFile.FileName);
+
+                //benzersiz bir dosya adı oluştur
+                var filename = Guid.NewGuid().ToString();
+
+                //Kayıt edilecek Dosyanın yolu
+                var saveLocation = Path.Combine(currenDirectory, "wwwroot/images", filename + extension);
+
+                //belirtilen konumda bir dosya oluştur
+                var stream = new FileStream(saveLocation, FileMode.Create);
+
+                //dosyayaı fiziksel olarak sunucuya yazar
+                model.ImageFile.CopyTo(stream);
+
+                model.ImageUrl = "/images/" + filename + extension;
+            }
+
+            _context.Products.Update(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value=_context.Products.Find(id);
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
